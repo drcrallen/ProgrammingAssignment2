d2baeeb4b800c948d7b2d2d80b3f921bc479cb45
Dr. Charles Allen <charles@allen-net.com>
2014-08-23 00:37:07 -0700

    [Programming Assignment 2] Week 3 - Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R - Wrap a matrix with a caching structure and allow
+##                 matrix inverse calculations on that structure
+## 
+## General use case
+##
+##  wrappedMatrix <- makeCacheMatrix(matrixOfInterest)
+##  cacheSolve(wrappedMatrix)
+##
+##  First wrap the matrix of interst in makeCacheMatrix
+##  Then call the `cacheSolve' instead of `solve'
+##
 
-## Write a short comment describing this function
+## makeCacheMatrix(matrixOfInterst)
+##  matrixOfInterst = The matrix that is desired to be inverted and cached
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## x is the matrix of interest
+    ## m is the cached mean value
+    m <- NULL
+    
+    ## getter / setter for x
+    set <- function(y){
+        x <<- y
+        m <<- NULL ## clear cache
+    }
+    get <- function() x
+    
+    ## getter / setter for mean
+    setmean <- function(mean) m<<-mean
+    getmean <- function() m
+    
+    list(set = set, get = get, setmean = setmean, getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(wrappedMatrix, ...)
+##  Check the wrappped matrix for a cached inverse and return it
+##  If no cached inverse is found, run the `solve' command
+##  Extra arguments are transparently passed to `solve'
+##
+## ****WARNING: the cached value uses only the FIRST set of arguments passed.
+## Any further calls to cacheSolve always assume the same arguments are passed.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getmean()
+    
+    ## Check for cache
+    if(!is.null(m)){
+        message("Found cached value for mean")
+        return(m)
+    }
+    
+    data <- x$get()
+    
+    ## Do the actual inverse
+    m <- solve(data, ...)
+    
+    ## cache for later
+    x$setmean(m)
+    m
 }
